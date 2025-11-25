0a37ce01fa48b787e4e5cf2e9e13a3f7fff2a103
Your Name <you@example.com>
2025-11-25 11:47:52 +0100

    added tests for post call

---
+++ cypress/e2e/API_Testing/PostCalls.cy.js
@@ -0,0 +1,75 @@
+const { expect } = require("chai")
+
+
+describe("api testing", () => {
+
+    it("Approach 1 - Hard coded json object", () => {
+        const requestBody = {
+            tourist_name: "Mike",
+            tourist_email: "mike23445@gmail.com",
+            tourist_location: "Paris"
+        }
+
+        cypress.request(
+            {
+                method: 'POST',
+                url: 'http://restapi.adequateshop.com/api/Tourist',
+                body: requestBody
+            })
+            .then((response) => {
+                expect(response.status).to.eq(201)
+                expect(response.body.tourist_name).to.eq("Mike")
+                expect(response.body.tourist_email).to.eq("mike23445@gmail.com")
+                expect(response.body.tourist_location).to.eq("Paris")
+            })
+    })
+
+    it("Approach 2 - Dynamically generating json object", () => {
+        const requestBody = {
+            tourist_name: Math.random().toString(5).substring(2),
+            tourist_email: Math.random().toString(5).substring(2) + "gmail.com",
+            tourist_location: "Paris"
+        }
+
+        cypress.request(
+            {
+                method: 'POST',
+                url: 'http://restapi.adequateshop.com/api/Tourist',
+                body: requestBody
+            })
+            .then((response) => {
+                expect(response.status).to.eq(201)
+                expect(response.body.tourist_name).to.eq(requestBody.tourist_name)
+                expect(response.body.tourist_email).to.eq(requestBody.tourist_email)
+                expect(response.body.tourist_location).to.eq(requestBody.tourist_location)
+            })
+    })
+
+
+    it("Approach 3 - using Fixture", () => {
+
+        cy.fixture('tourist').then((data) => {
+            const requestBody = data;
+
+
+            cy.request(
+                {
+                    method: 'POST',
+                    url: 'http://restapi.adequateshop.com/api/Tourist',
+                    body: requestBody
+                })
+
+                .then((response) => {
+                    expect(response.status).to.eq(201)
+                    expect(response.body.tourist_name).to.eq(requestBody.tourist_name)
+                    expect(response.body.tourist_email).to.eq(requestBody.tourist_email)
+                    expect(response.body.tourist_location).to.eq(requestBody.tourist_location)
+
+                    expect(response.body).has.property('tourist_email', requestBody.tourist_email)
+                    expect(response.body).to.have.property('tourist_email', requestBody.tourist_email)
+                })
+        })
+
+    })
+
+})
